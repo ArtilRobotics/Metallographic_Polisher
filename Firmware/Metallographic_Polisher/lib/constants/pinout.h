a68c2d55262afa309b4959ab03ed822202e6d685
StevenCuripallo98 <stevencuripallo@hotmail.com>
2023-10-03 15:08:09 -0500

    feat: inicial files

---
+++ Firmware/Metallographic_Polisher/lib/constants/pinout.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define Encoder1 A0
+#define Encoder2 A1
+#define Encoder3 A2
+#define Rele_Amola A3
+#define Rele_Lijas A4
+#define Rele_Refrige A5
+
+#define MotorXA 2
+#define MotorXB 3
+#define MotorYA 4
+#define MotorYB 5
+#define MotorZA 6
+#define MotorZB 7
+#define MotorUsilloA 8
+#define MotorUsilloB 9
+#define MotorLijaA 10
+#define MotorLijaB 11
+#define Buzzer 12
+#define FC_X 14
+#define FC_Y 15
+#define FC_Z 16
+#define SDA 20
+#define SCL 21
+
+
+
